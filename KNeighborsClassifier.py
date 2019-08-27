78dc49f6c846ecc2eb349dde4235ea2fca8df97a
ssmoon0828 <ssmoon0828@gmail.com>
2019-08-27 15:05:59 +0900

    KNeighborsClassifier 알고리즘 추가, train_test_split 코드(미완) 추가

---
+++ KNeighborsClassifier.py
@@ -0,0 +1,39 @@
+#%%
+import numpy as np
+import pandas as pd
+
+#%%
+class knn:
+    import numpy as np
+    import pandas as pd
+    
+    def __init__(self, k = 3):
+        self.k = k
+    
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def predict(self, X_test):
+        self.X_test = X_test
+        pred_list = []
+        
+        for test_point in self.X_test:
+            dist_square_list = []
+            
+            for train_point in self.X_train:
+                diff_vector = test_point - train_point
+                dist_square = np.dot(diff_vector, diff_vector)
+                dist_square_list.append(dist_square)
+            
+            dist_square_df = pd.DataFrame({'y_label' : self.y_train,
+                                           'dist_square' : dist_square_list})
+            dist_square_df = dist_square_df.sort_values('dist_square')        
+            label_index, label_counts = np.unique(dist_square_df['y_label'][: self.k + 1], return_counts = True)
+            most_counts_label = label_index[np.argmax(label_counts)]
+            pred_list.append(most_counts_label)
+        
+        return pred_list
+    
+    def score(self, X_test, y_test):
+        return np.mean(knn.predict(X_test) == y_test)
---
+++ train_test_split.py
@@ -0,0 +1,7 @@
+#%%
+import numpy as np
+
+#%%
+def train_test_split(X, y, train_ratio = 0.7):
+    
+    return X_train, X_test, y_train, y_test
