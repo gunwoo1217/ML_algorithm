4b5df23b03f0d447460777e650e94921cb4c8edb
ssmoon0828 <ssmoon0828@gmail.com>
2019-09-04 15:12:55 +0900

    train, test split 코드 추가

--- train_test_split.py
+++ train_test_split.py
@@ -1,7 +1,22 @@
-#%%
 import numpy as np
 
-#%%
 def train_test_split(X, y, train_ratio = 0.7):
     
-    return X_train, X_test, y_train, y_test
+    if len(X) == len(y):
+        train_size = round(len(y) * train_ratio)
+        
+        random_idx = np.arange(len(y))
+        np.random.shuffle(random_idx)
+        
+        train_idx = random_idx[: train_size]
+        test_idx = random_idx[train_size :]
+        
+        X_train = X[train_idx]
+        X_test = X[test_idx]
+        y_train = y[train_idx]
+        y_test = y[test_idx]
+        
+        return X_train, X_test, y_train, y_test
+    
+    else:
+        print('X와 y의 길이가 맞지 않습니다.')
