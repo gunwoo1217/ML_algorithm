5c425e456c5e09b43414165066a72f52d525c054
서상문 <33209487+ssmoon0828@users.noreply.github.com>
2020-02-17 11:25:16 +0900

    Update and rename k_neighbors_classifier.py to knn_classifier.py

---
+++ code/knn_classifier.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+class knn_regressor:
+    
+    def __init__(self, k = 3):
+        self.k = k
+        
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def predict(self, X_test):
+        self.X_test = X_test
+        
+        y_pred = []
+        
+        for test_point in self.X_test:
+            diff_square_list = []
+            
+            for train_point in self.X_train:
+                diff_vector = test_point - train_point
+                diff_square = np.dot(diff_vector, diff_vector)
+                diff_square_list.append(diff_square)
+            
+            diff_square_list = np.array(diff_square_list)
+            near_idx = np.argsort(diff_square_list)
+            
+            y_pred.append(np.mean(self.y_train[near_idx][:self.k]))
+        
+        return np.array(y_pred)
+    
+    def score(self, X_test, y_test):
+        return np.mean(self.predict(X_test) == y_test)
